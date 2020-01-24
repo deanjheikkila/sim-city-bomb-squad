acffe496625cb822d81aa5326f71492f28300ea0
deanjheikkila <deanjheikkila@gmail.com>
2020-01-24 15:06:48 -0800

    style done, js linked

--- bomb.js
+++ bomb.js
@@ -1,1 +1,36 @@
 console.log('loaded!');
+
+// wire event listener:
+//      changes wire img**
+//      disables future clicks on that wire
+// iterate over wires
+//      assign cut/no cut
+// write win condition/function
+    // find out if all relevant wires have been cut
+    // timer stops
+    // music + cheers
+    // disable wire cutting
+// write lose condition/function
+//      check if wrong wire cut
+//      check if timer goes to zero
+//      background changes**
+//      timer stops
+//      play boom sound**
+//      button shows 'reset'
+//      750 millisecond delay
+// wireclick function
+//      img change
+//      buzzer
+//      check the win function
+//      disable wire click
+// check win value
+    // Return true or false
+    // check the state of wires (iteration)
+// reset function
+    // set background
+    // reset wire images
+    // reset wire state
+    // reset timer
+    // disable reset button
+    // sirens
+// game init ?
